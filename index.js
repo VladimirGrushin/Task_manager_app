74b2f199903c2341ce529eec14a0fdbc15eaa736
alnubvar <alnub_work@mail.ru>
2025-05-30 22:33:35 +0300

    Fix startup configuration: update Procfile.dev and JS/CSS build setup

---
+++ index.js
@@ -0,0 +1,14 @@
+var $7Tu7o$hotwiredturborails = require("@hotwired/turbo-rails");
+var $7Tu7o$hotwiredstimulus = require("@hotwired/stimulus");
+var $7Tu7o$hotwiredstimuluswebpackhelpers = require("@hotwired/stimulus-webpack-helpers");
+
+
+
+
+(0, $7Tu7o$hotwiredturborails.Turbo).start();
+const $3b724a02d232d001$var$application = (0, $7Tu7o$hotwiredstimulus.Application).start();
+const $3b724a02d232d001$var$context = undefined("./controllers", true, /\.js$/);
+$3b724a02d232d001$var$application.load((0, $7Tu7o$hotwiredstimuluswebpackhelpers.definitionsFromContext)($3b724a02d232d001$var$context));
+
+
+//# sourceMappingURL=index.js.map
