265c12ed24f25ad23caef2e361918d7fb5c07c9d
alnubvar <alnub_work@mail.ru>
2025-05-31 04:23:11 +0300

    change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Tasks routes
   resources :tasks do
     collection do
       get "admin", to: "tasks#admin_index", as: :admin
     end
   end
 
-  resources :categories, only: [ :index ]
-  root to: redirect("/tasks")
+  # Categories routes
+  resources :categories, only: [:index]
+
+  # Root route - changed from redirect to direct action
+  root "tasks#index"
 end
